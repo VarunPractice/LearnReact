76bd486e504742e4eebdfefce058087633a1e334
Aarush@122015 <you@example.com>
2024-03-20 04:06:51 +0530

    Adding children concept in container class

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Container from './Components/Container'
 
 function App() 
 {
@@ -11,12 +12,18 @@ function App()
     let msg= healthyItems.length===0 && <h3>im hungry</h3>;
     return(
       <>
+      <Container>
         <h1>Healthy Food</h1>
         {msg}
         <ul className="list-group">
           {healthyItems.sort().map(item=><li key={item} className="list-group-item">{item}</li>)}
           </ul> 
-     </>
+     </Container>
+       <Container>
+       <p>
+        the above items are healthy food items , good for ur health</p>
+    </Container>
+    </>
     )
     }
 export default App
